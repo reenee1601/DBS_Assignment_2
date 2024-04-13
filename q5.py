ca5c6394f48eec37263a17abe09459d20f32f8ab
Harini Parthasarathy <87818138+reenee1601@users.noreply.github.com>
2024-04-13 23:54:44 +0800

    qn5

--- q5.py
+++ q5.py
@@ -1,9 +1,36 @@
-import sys 
 from pyspark.sql import SparkSession
-# you may add more import if you need to
+from pyspark.sql.functions import explode, collect_list, size
+from itertools import combinations
 
-# don't change this line
-hdfs_nn = sys.argv[1]
+# Create a SparkSession
+spark = SparkSession.builder \
+    .appName("Co-Cast Analysis") \
+    .getOrCreate()
 
-spark = SparkSession.builder.appName("Assigment 2 Question 5").getOrCreate()
-# YOUR CODE GOES BELOW
+# Load the Parquet file into a DataFrame
+df = spark.read.parquet("hdfs://ip-172-31-94-60.ec2.internal:9000/assignment2/part2/input/tmdb_5000_credits.parquet")
+
+# Split the cast column into individual actors/actresses
+df_cast = df.withColumn("actor", explode(col("cast"))).select("movie_id", "title", "actor")
+
+# Collect the list of actors/actresses for each movie
+df_grouped = df_cast.groupBy("movie_id", "title").agg(collect_list("actor").alias("cast_list"))
+
+# Generate pairs of actors/actresses for each movie
+df_pairs = df_grouped.withColumn("actor_pairs", explode(combinations(col("cast_list"), 2))) \
+                     .select("movie_id", "title", col("actor_pairs").alias("actors"))
+
+# Count the occurrences of each pair
+df_count = df_pairs.groupBy("actors").agg(size(collect_list("movie_id")).alias("num_movies"))
+
+# Filter out pairs that appear in at least 2 movies
+df_filtered = df_count.filter(col("num_movies") >= 2)
+
+# Format the output as RDD
+rdd_output = df_filtered.rdd.map(lambda row: (row['actors'][0], row['actors'][1], row['num_movies']))
+
+# Save the output as Parquet files
+rdd_output.saveAsTextFile("hdfs://ip-172-31-94-60.ec2.internal:9000/assignment2/output/question5/")
+
+# Stop the SparkSession
+spark.stop()
